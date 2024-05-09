b5be18a62ddffc2c3c0e0e574434b7da8cdbb540
Akseli Palmer <akseli@toddsden.com>
2024-05-09 19:47:27 -0300

    students bannar page

---
+++ src/app/student/page.tsx
@@ -0,0 +1,11 @@
+export default function Student() {
+  return (
+    <div className="py-4 px-2">
+      <div className="sm:text-center">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Material
+        </h2>
+      </div>
+    </div>
+  );
+}
